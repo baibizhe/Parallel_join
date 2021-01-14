1e085242da4706411f5d214cf6501443f317d610
baibizhe <bbzatyddounan@gmail.com>
2021-01-14 17:29:10 +0800

    modified

--- join-omp.c
+++ join-omp.c
@@ -1,14 +1,5 @@
 // ------------
-// This code is provided solely for the personal and private use of
-// students taking the CSC367H5 course at the University of Toronto.
-// Copying for purposes other than this use is expressly prohibited.
-// All forms of distribution of this code, whether as given or with
-// any changes, are expressly prohibited.
-//
-// Authors: Bogdan Simion, Alexey Khrabrov
-//
-// All of the files in this directory and all subdirectories are:
-// Copyright (c) 2019 Bogdan Simion
+
 // -------------
 
 #include <stdio.h>
@@ -48,7 +39,6 @@ int main(int argc, char *argv[])
 	double t_start = omp_get_wtime();
     int count = -1;
    // (void)join_f;
-	//TODO: parallel join using OpenMP
     if(opt_replicate){
         //method 1, split on smaller array
 	int s_count = MAX(students_count, tas_count);
--- join.c
+++ join.c
@@ -1,15 +1,3 @@
-// ------------
-// This code is provided solely for the personal and private use of
-// students taking the CSC367H5 course at the University of Toronto.
-// Copying for purposes other than this use is expressly prohibited.
-// All forms of distribution of this code, whether as given or with
-// any changes, are expressly prohibited.
-//
-// Authors: Bogdan Simion, Alexey Khrabrov
-//
-// All of the files in this directory and all subdirectories are:
-// Copyright (c) 2019 Bogdan Simion
-// -------------
 
 #include <assert.h>
 #include <stdio.h>
